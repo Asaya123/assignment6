5e8c5aee09539c6add0de75cd995779d38828883
Asaya123 <as24csb0b88@student.nitw.ac.in>
2025-02-18 22:35:11 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,28 @@
+
+
+let calculator = document.getElementById("calculator");
+
+function clearScreen() {
+    document.getElementById('given').value = '';
+}
+
+function addgiven(value) {
+    document.getElementById('given').value += value;
+}
+
+function calculate() {
+    let ans = document.getElementById('given');
+    try {
+        const temp=Function('return ' + ans.value);
+        const ans1=temp();
+        if(ans1==Infinity){
+            ans.value='∞';
+            setTimeout(clearScreen, 2000);
+        }
+        else{
+            ans.value=ans1;
+        }
+    } catch (error) {
+        ans.value = 'Error';
+    }
+}
